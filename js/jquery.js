e5110183958dc2abcb53acd5d7023c6713446ab0
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-06-25 10:16:40 +0200

    css, hover

--- js/jquery.js
+++ js/jquery.js
@@ -2,8 +2,24 @@
 $(document).ready(function(){
     $('.two').hide();
 
-    $('button').click(function(){
+    $('#button-p').click(()=>{
         $('.one').hide();
         $('.two').show();
     })
+
+    $('.hover-me').css("width","300px")
+    $('.hover-me').css("height","300px")
+    $('.hover-me').css("background-color","red")
+
+    $('.hover-me').hover(function(){
+        $(this).css("background-color","green")
+    })
+
+    $('#button-yellow').click(()=>{
+    $('.hover-me').css("background-color","yellow")
+
+    })
+
+
+
 })
