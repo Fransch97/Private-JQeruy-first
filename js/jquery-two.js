9f1484fe3f0367aa010d09fb3d90762713afe693
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-06-25 11:54:26 +0200

    chaining

--- js/jquery-two.js
+++ js/jquery-two.js
@@ -4,6 +4,12 @@ $(document).ready(()=>{
         $(this).hide('slow', ()=>{alert("Danke bin mal weg")})
     })
 
+    $('#chain').click(function(){
+        setInterval(()=>{
+            $(this).css('background-color','black').css('color','white').slideUp(1000).slideDown(1000)
+        })
+    })
+
 
 
 })
