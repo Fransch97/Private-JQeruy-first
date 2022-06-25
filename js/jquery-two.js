43aa8e74bbc7fe622ae4b6afb8c0eb3f90cafb5a
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-06-25 12:07:39 +0200

    get

--- js/jquery-two.js
+++ js/jquery-two.js
@@ -1,15 +1,21 @@
 $(document).ready(()=>{
 //  console.log('ready')
     $('#callback').click(function(){
-        $(this).hide('slow', ()=>{alert("Danke bin mal weg")})
+        $(this).hide('slow', ()=>{alert("Danke bin mal weg")});
     })
 
     $('#chain').click(function(){
         setInterval(()=>{
-            $(this).css('background-color','black').css('color','white').slideUp(1000).slideDown(1000)
+            $(this).css('background-color','black').css('color','white').slideUp(1000).slideDown(1000);
         })
     })
 
+    $('#get').click(()=>{
+        console.log( $('span').text());
+    })
 
+    $('#la').click(()=>{
+        console.log($('input').val())
+    })
 
 })
